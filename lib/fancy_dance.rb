a1da403c1707ef8e0d961b447e5d315923dec04b
Denise Henderson <denise@henderson3.us>
2020-08-08 12:59:59 +0000

    Automatically backed up by Learn

--- lib/fancy_dance.rb
+++ lib/fancy_dance.rb
@@ -18,3 +18,12 @@ module FancyDance
     end
 
   end #module InstanceMethods
+
+  module ClassMethods
+
+    def metadata
+      "This class produces objects that love to dance."
+    end
+
+  end
+end
